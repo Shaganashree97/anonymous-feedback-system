d5b78a386972f5fe09d1774de99e514b9dddb2c9
Shaganashree97 <shaganashree.pl@gmail.com>
2024-01-26 19:01:22 +0530

    Deleted all

--- backend/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\FeedbackController;
-
-Route::prefix('feedbacks')->group(function () {
-    Route::get('/', [FeedbackController::class, 'index'])->name('api.feedback.index');
-    Route::post('/', [FeedbackController::class, 'store'])->name('api.feedback.store');
-});
